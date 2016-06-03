fd302b54600c7d4b00af0efec754534856bce8e2
Daniel Oliveira <daniel.oliveira079@gmail.com>
2016-06-02 23:41:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-var port = process.ENV.PORT || 4000
+var port = process.env.PORT || 4000
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
